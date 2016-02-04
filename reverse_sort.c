a39b8133df7cc6c63605538642f4cc2bc20d9d50
KabirSingh114 <kabirs@umich.edu>
2016-02-03 23:08:34 -0500

    Update reverse_sort.c

--- reverse_sort.c
+++ reverse_sort.c
@@ -1,1 +1,13 @@
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "sort.h"
 
+int compare(int a, int b) {
+	    return b-a;
+}
+
+int compare_q(int *a, int *b) {
+	return *b - *a;
+}
